14784da4eae259a71924dfadc224a9444926b173
Juan_Carlos <jc.zarate309@gmail.com>
2019-05-09 19:40:45 +0200

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require('./config/config.js');
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -10,34 +11,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
-
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        
-        res.json({persona: body})
-    }
-})
-
-app.put('/usuario/:id', function (req, res) {
-    
-    let id = req.params.id
-    res.json({id:id})
-})
-
-app.delete('/usuario', function (req, res) {
-  res.json('delete usuario')
-})
+app.use( require('./routes/usuario'));
  
+
+
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useCreateIndex: true},
+                (err, resp) => {
+
+        if (err) throw err;
+        console.log('Base de datos ONLINE');
+        }
+);
+
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en puerto: ', process.env.PORT);
 })
