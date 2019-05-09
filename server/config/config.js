2d42a1951dc25e639875c98929e0a99d4a568f8c
Juan_Carlos <jc.zarate309@gmail.com>
2019-05-09 20:11:54 +0200

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
     
 } else {
-    urlDB = 'mongodb+srv://jzaratei:030986@cluster0-nl7av.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
